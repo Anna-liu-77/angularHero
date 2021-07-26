af79db7fee2173649681ebe0cb61c0afae97782a
Wenqi <wenqi.a.liu@accentrue.com>
2021-07-26 17:17:01 +0800

    created heroes detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template:`
+  // <h1>Hello World</h1>
+  // `,
   styleUrls: ['./app.component.css']
+  // styles:[
+  //   `h1{
+  //     color:blue;
+  //   }
+  //   `
+  // ]
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Tour of Heroes';
 }
